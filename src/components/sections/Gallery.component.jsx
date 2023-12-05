375f2ba68ed97bbc4503d3dbe134cec6612b2490
Joel Viloria <viloriajoel07@github.com>
2023-12-05 09:00:44 -0500

    chage routes images

--- src/components/sections/Gallery.component.jsx
+++ src/components/sections/Gallery.component.jsx
@@ -1,5 +1,4 @@
 import { images } from "../../resource/images";
-import GalleryItem from "../GalleryItem.component";
 
 export default function Gallery() {
   return (
@@ -12,10 +11,17 @@ export default function Gallery() {
         <div className="mt-6 flex flex-wrap justify-center gap-6 px-8 md:px-0">
           {images.map((img) => {
             return (
-              <GalleryItem
+              <div
                 key={img.id}
-                image={img.imageSrc}
-              />
+                className="cursor-pointer flex items-center overflow-hidden rounded-lg shadow-lg relative w-full md:w-80 h-auto md:h-64 border-2 border-white/10"
+              >
+                <img
+                  className="w-full h-auto object-cover hover:scale-110 transition-all duration-300 bg-bottom"
+                  src={`/public/gallery/${img.imageSrc}`}
+                  alt="Garelly Item"
+                  loading="lazy"
+                />
+              </div>
             );
           })}
         </div>
--- src/resource/images.js
+++ src/resource/images.js
@@ -1,66 +1,66 @@
 export const images = [
   {
     id: 1,
-    imageSrc: "/src/assets/gallery/IMG_20210521_175046.jpg",
+    imageSrc: "IMG_20210521_175046.jpg",
   },
   {
     id: 4,
-    imageSrc: "/src/assets/gallery/IMG_20211112_122329.jpg",
+    imageSrc: "IMG_20211112_122329.jpg",
   },
   {
     id: 6,
-    imageSrc: "/src/assets/gallery/IMG_20211128_145856.jpg",
+    imageSrc: "IMG_20211128_145856.jpg",
   },
   {
     id: 7,
-    imageSrc: "/src/assets/gallery/IMG_20220106_165217.jpg",
+    imageSrc: "IMG_20220106_165217.jpg",
   },
   {
     id: 8,
-    imageSrc: "/src/assets/gallery/IMG_20220106_170048.jpg",
+    imageSrc: "IMG_20220106_170048.jpg",
   },
   {
     id: 9,
-    imageSrc: "/src/assets/gallery/IMG_20220106_171003.jpg",
+    imageSrc: "IMG_20220106_171003.jpg",
   },
   {
     id: 10,
-    imageSrc: "/src/assets/gallery/IMG_20220106_171414.jpg",
+    imageSrc: "IMG_20220106_171414.jpg",
   },
   {
     id: 11,
-    imageSrc: "/src/assets/gallery/IMG_20220115_110056.jpg",
+    imageSrc: "IMG_20220115_110056.jpg",
   },
   {
     id: 12,
-    imageSrc: "/src/assets/gallery/IMG_20220115_110449.jpg",
+    imageSrc: "IMG_20220115_110449.jpg",
   },
   {
     id: 13,
-    imageSrc: "/src/assets/gallery/IMG_20220629_212834.jpg",
+    imageSrc: "IMG_20220629_212834.jpg",
   },
   {
     id: 14,
-    imageSrc: "/src/assets/gallery/IMG_20230828_143915.jpg",
+    imageSrc: "IMG_20230828_143915.jpg",
   },
   {
     id: 15,
-    imageSrc: "/src/assets/gallery/IMG_20230828_143922.jpg",
+    imageSrc: "IMG_20230828_143922.jpg",
   },
   {
     id: 16,
-    imageSrc: "/src/assets/gallery/IMG_20230926_154911.jpg",
+    imageSrc: "IMG_20230926_154911.jpg",
   },
   {
     id: 17,
-    imageSrc: "/src/assets/gallery/IMG_20220115_101945.jpg",
+    imageSrc: "IMG_20220115_101945.jpg",
   },
   {
     id: 18,
-    imageSrc: "/src/assets/gallery/IMG_20220607_162801.jpg",
+    imageSrc: "IMG_20220607_162801.jpg",
   },
   {
     id: 19,
-    imageSrc: "/src/assets/gallery/IMG_2023.jpeg",
+    imageSrc: "IMG_2023.jpeg",
   },
 ];
